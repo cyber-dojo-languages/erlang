5efa44763df113ddf102b08ad4da346fd9e28ebf
Jon Jagger <jon@jaggersoft.com>
2018-02-25 17:46:34 +0000

    added hyperlink to readme version number

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Build Status](https://travis-ci.org/cyber-dojo-languages/erlang.svg?branch=master)
 
-Version=20
+[Version=20](https://github.com/cyber-dojo-languages/erlang/blob/master/check_version.sh)
--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/erlang sh -c 'erl -eval "erlang:display(erlang:system_info(otp_release)), halt()." -noshell')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
