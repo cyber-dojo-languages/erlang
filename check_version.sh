66cc081daf66ff8dee69e9328f4202e734a88f21
Jon Jagger <jon@jaggersoft.com>
2022-06-24 20:25:23 +0100

    Upgrade to v25

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=23
+readonly EXPECTED=25
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'erl -eval "erlang:display(erlang:system_info(otp_release)), halt()." -noshell')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
